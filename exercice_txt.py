8088b25a887c0e4a0fe83af226b073628ecc2ba1
cKouT <mp.ckout@gmail.com>
2018-10-13 20:11:02 +0400

    Ajout des primers fichiers crées

---
+++ exercice_txt.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+"""
+This is an example script.
+
+It seems that it has to have THIS docstring with a summary line, a blank line
+and sume more text like here. Wow.
+"""
+name = "éric"
+name = name.title()
+
+print("Hello " + name + ", would you like to learn some Python today?")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print('Albert Einstein once said, “A person who never made a mistake never tried anything new.”')
+
+famous_person = "albert einstein"
+quote = "A person who never made a mistake never tried anything new."
+
+message = famous_person.title() + 'once said "' + quote + '"'
+
+famous_person = "\t salvador dali \n"
+
+print(famous_person.lstrip() + famous_person.rstrip() + famous_person.strip())
+
---
+++ numbers.py
@@ -0,0 +1,15 @@
+add = 2 + 3
+mult = 4 * 4
+div = 8 / 2
+
+print(str(add))
+print(str(mult))
+print(str(div))
+
+print(5 + 3)
+print(4 * 2)
+print(16 / 2)
+print(1 + 1 * 2 * 2 + 3)
+
+favorite_number = 10
+print(favorite_number)
