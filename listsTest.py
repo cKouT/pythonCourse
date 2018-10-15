9bc8c3f4723b7c1d6a771007ed940734ff8718cc
cKouT <mp.ckout@gmail.com>
2018-10-15 10:04:05 +0200

    ajout du listsTest.py + espaces

---
+++ listsTest.py
@@ -0,0 +1,7 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+print(bicycles[0])
+print(bicycles[1].title())
+print(bicycles[2].upper())
+print(bicycles[3].lower())
